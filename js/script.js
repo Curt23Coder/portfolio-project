7060255e974b6fdafdedf11768eabc53b940011c
Curtis Myers <curt.myers@aol.com>
2021-09-07 03:18:57 -0400

    New Project

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+$(".img-1").mouseover(function(){
+$(".github-hover1").animate({
+opacity: "1"
+});
+$(".img-1").mouseleave(function(){
+    $(".github-hover1").animate({
+    opacity: "0"
+    });
+    
+});
+});
+});
+    $(".img-2").mouseover(function(){
+    $(".github-hover2").animate({
+    opacity: "1"
+    });
+});
+    $(".img-2").mouseleave(function(){
+        $(".github-hover2").animate({
+        opacity: "0"
+        });
+        
+    });
+    $(".img-3").mouseover(function(){
+        $(".github-hover3").animate({
+        opacity: "1"
+        });
+    });
+        $(".img-3").mouseleave(function(){
+            $(".github-hover3").animate({
+            opacity: "0"
+            });
+            
+        });
+    
